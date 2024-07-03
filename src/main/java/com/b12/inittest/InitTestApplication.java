237a964efe75767fbbdaa083cc9e9b9925928cc5
tichall <0817frog@gmail.com>
2024-07-03 17:59:19 +0900

    fitst commit

---
+++ src/main/java/com/b12/inittest/InitTestApplication.java
@@ -0,0 +1,13 @@
+package com.b12.inittest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InitTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InitTestApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/b12/inittest/common/config/JpaAuditingConfig.java
@@ -0,0 +1,9 @@
+package com.b12.inittest.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaAuditingConfig {
+}
